6ab5a1fee8611ac55f03761e57321d530a2648ad
Florian Svendsen <floriansvendsen@gmail.com>
2026-08-03 14:41:56 +0200

    Removed the sticky headers, didn't work as expected

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // installed and opens instantly; weather/geocoding API calls always go
 // to the network untouched (forecasts should never be served stale).
 
-const CACHE_NAME = "pilsindex-shell-v40";
+const CACHE_NAME = "pilsindex-shell-v46";
 const APP_SHELL = [
     "./",
     "./index.html",
