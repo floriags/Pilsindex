193e3c56b5cd1a2fb2406d489d4343981fdb4ace
Florian Svendsen <floriansvendsen@gmail.com>
2026-08-04 15:25:53 +0200

    Added hour score to final index

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // installed and opens instantly; weather/geocoding API calls always go
 // to the network untouched (forecasts should never be served stale).
 
-const CACHE_NAME = "pilsindex-shell-v67";
+const CACHE_NAME = "pilsindex-shell-v68";
 const APP_SHELL = [
     "./",
     "./index.html",
